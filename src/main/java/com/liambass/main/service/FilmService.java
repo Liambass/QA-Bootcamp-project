35264e55983a16946352a33fb7f072499b36028d
Liambass <Liam.Purkiss.spam@gmail.com>
2021-10-01 13:21:50 +0100

    added barebones controller layer

--- src/main/java/com/liambass/main/service/FilmService.java
+++ src/main/java/com/liambass/main/service/FilmService.java
@@ -1,9 +1,11 @@
 package com.liambass.main.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.liambass.main.repo.FilmRepo;
 
+@Service
 public class FilmService {
 
 	private FilmRepo repo;
