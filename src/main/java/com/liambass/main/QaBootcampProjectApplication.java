c0fa2db1c055a1f75cd2aa3767e8a04cf0bb3640
Liambass <Liam.Purkiss.spam@gmail.com>
2021-10-01 09:40:07 +0100

    first commit

---
+++ src/main/java/com/liambass/main/QaBootcampProjectApplication.java
@@ -0,0 +1,13 @@
+package com.liambass.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaBootcampProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QaBootcampProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/liambass/main/QaBootcampProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liambass.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaBootcampProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
